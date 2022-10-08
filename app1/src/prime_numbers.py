a408693d0316e55661d226991c82a378f8f85b41
Cuuuuube <98660562+Cuuuuube@users.noreply.github.com>
2022-10-08 16:48:17 +0200

    undo

--- app1/src/prime_numbers.py
+++ app1/src/prime_numbers.py
@@ -3,18 +3,19 @@ Command: prime_numbers
 Return a list of all the prime numbers inferior or equal to n
 """
 def prime_numbers(n):
-
-    if n < 2:
-        return False
+    def is_prime(n):
+        if n < 2:
+            return False
+        for i in range(2,n):
+            if (n%i) == 0:
+                return False
+        return True
 
     result = []
     for i in range(n+1):
-        for i in range(2,n):
-            if (n%i) == 0:
-                break
-        result.append(i)
+        if is_prime(i):
+            result.append(i)
     return result
-
 
 """
 Command: sum_prime_numbers
